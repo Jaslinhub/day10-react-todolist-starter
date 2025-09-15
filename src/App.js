f9ac69a04e56fcf63d603604f4f2c6cb3e5a6d84
Jasmine <jasmine.lin@oocl.com>
2025-09-15 11:52:14 +0800

    feat:add todo list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,63 @@ import TodoList from "./components/TodoList";
 import {initialState, todoReducer} from "./reducers/todoReducer";
 import {TodoContext} from "./contexts/TodoContext";
 import {TodoGenerator} from "./components/TodoGenerator";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useParams} from "react-router";
 
+
+function DefaultLayout() {
+    return <>
+    <header>
+        <nav>
+            <ul>
+                <li><NavLink to={'/'}>Home</NavLink></li>
+                <li ><NavLink to={'/todos'}> To do List </NavLink></li>
+                <li><NavLink to={'/about'}>About</NavLink></li>
+            </ul>
+        </nav>
+    </header>
+        <main>
+            <h1>xxx</h1>
+            <Outlet/>
+        </main>
+        </>;
+}
+
+function ErrorPage() {
+    return <h1>Error Page</h1>;
+}
+
+function TodoDetail() {
+    const {key}=useParams();
+    return <h1>This is :{key} Detail </h1>;
+}
+
+const routes=[{
+    path: '/',
+    element: <DefaultLayout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+        {
+            path: '',
+            element: <h1>Home page</h1>
+        }, {
+            path: 'todos',
+            element:<TodoList/>
+
+        },
+        {
+            path: 'todos/:key',
+            element:<TodoDetail/>
+
+        },{
+            path: 'about',
+            element: <h1>About page</h1>
+
+        }]
+}]
+
+
+
+const router=createBrowserRouter(routes);
 function App() {
   // the Hooks API manage component data state
   const [state, dispatch] = useReducer(todoReducer, initialState);
@@ -12,10 +68,16 @@ function App() {
 
   return (
     <div className="App">
-      <TodoContext.Provider value={value}>
+        <div>
+            <TodoContext.Provider value={value}>
+            <RouterProvider router={router}></RouterProvider>
+                <TodoGenerator dispatch={dispatch} length={state.length}/>
+            </TodoContext.Provider>
+        </div>
+     {/* <TodoContext.Provider value={value}>
         <TodoList/>
       </TodoContext.Provider>
-        <TodoGenerator dispatch={dispatch} length={state.length}/>
+        <TodoGenerator dispatch={dispatch} length={state.length}/>*/}
     </div>
   );
 }
